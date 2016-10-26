c2d7f181387740f32a841d8fd9f275462708e41d
idopes <idosamet1@gmail.com>
2016-10-26 21:44:07 +0300

    reads logs into nodes, but does not do i correctly to do so read http://stackoverflow.com/questions/17352321/python-splitting-string-by-parentheses

--- reader.py
+++ reader.py
@@ -4,21 +4,32 @@ Created on Wed Oct 26 17:26:53 2016
 
 @author: user
 """
-
+from log import Log
 
 class LogsReader:
 
     rawTxt = ""
-    txtDevidedInLines = ""
+    txtDevidedInLines = []
+    fullOopLogs = []
     oopLogs = []
     
+
     def __init__(self, filename):
          file = open(filename, 'r')
          self.rawTxt = file.read()
          self.txtDevidedInLines = self.rawTxt.splitlines()
+         self.init_fullOop()
+         self.define_oop()
          
-
+    def init_fullOop(self):
+        for i in self.txtDevidedInLines[10:]:
+            string_log = i.split()
+            self.fullOopLogs.append(Log(string_log,0))
+            
+    def define_oop(self):
+        for i in self.fullOopLogs:
+            if i.adnet == "none" or i.adnet == "-":
+                self.oopLogs.append(Log(i,1))
          
 x = LogsReader("short_log.txt")
-for i in x.txtDevidedInLines:
-    print(i)
+j = 10
